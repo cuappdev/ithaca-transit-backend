63399d86ca9a1257764edd56a009e10d92a74114
Jma353 <jma353@cornell.edu>
2017-08-01 19:40:56 -0700

    Resolve flow errors

--- src/Raptor.js
+++ src/Raptor.js
@@ -1,11 +1,8 @@
 // @flow
-import Location from './models/Location';
-import Route from './models/Route';
 import Stop from './models/Stop';
 
 class Raptor {
   stops: Array<Stop>;
-  routes: Array<Route>;
   // TODO
 }
 
