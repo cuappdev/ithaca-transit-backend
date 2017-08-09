d70bb3d2c50259fe048c575b19ff91cc751debcf
Jma353 <jma353@cornell.edu>
2017-08-08 19:11:33 -0700

    I see the finish line

--- src/models/Bus.js
+++ src/models/Bus.js
@@ -1,5 +1,7 @@
 // @flow
 import Path from './Path';
+import TCATConstants from '../utils/TCATConstants';
+import TimedStop from './TimedStop';
 
 /**
  * A bus line hat runs at various times during the week.  The busline can
@@ -12,8 +14,28 @@ class Bus {
   lineNumber: number;
 
   constructor (paths: Array<Path>, lineNumber: number) {
-    this.paths = paths;
     this.lineNumber = lineNumber;
+    this.paths = [];
+    for (let i = 0; i < paths.length; i++) {
+      const path = paths[i];
+      for (let j = path.days.start; j <= path.days.end; j++) {
+        // Modifications to account for the fact that buses run on different
+        // days, and to fit said paths into the framework of the Raptor algo
+        const days = {start: j, end: j};
+        const augmentedTimedStops = path.timedStops.map(timedStop => {
+          let timedStopClone = TimedStop.clone(timedStop);
+          timedStopClone.time = timedStop.time + j * TCATConstants.DAY;
+          return timedStopClone;
+        });
+        const augmentedStartTime = j * TCATConstants.DAY + path.startTime;
+
+        this.paths.push(new Path(
+          days,
+          augmentedTimedStops,
+          augmentedStartTime
+        ));
+      }
+    }
   }
 }
 
---
+++ src/utils/TCATConstants.js
@@ -0,0 +1,11 @@
+// @flow
+
+const MINUTE = 60;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+
+export default {
+  MINUTE,
+  HOUR,
+  DAY
+};
