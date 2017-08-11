16838e771a2d933552788269e1b980a6010637e1
Jma353 <jma353@cornell.edu>
2017-08-10 20:08:59 -0700

    retainLines

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,6 @@ fs.readdirSync('node_modules')
   });
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
   entry: [path.join(__dirname, 'src/server.js')],
   context: __dirname,
   node: {
