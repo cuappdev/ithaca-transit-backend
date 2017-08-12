82b26cba7f2557f6c25f4f005fba03828d1d9722
Shiv Roychowdhury <sr856@cornell.edu>
2017-08-12 02:37:34 -0700

    Fix bugs in KML coordinate parsing

--- src/utils/KmlUtils.js
+++ src/utils/KmlUtils.js
@@ -2,40 +2,17 @@
 import Location from '../models/Location';
 
 const locationsFromPlacemark = (placemark: string): Array<Location> => {
-	var regex = /-?[\d|.|e|E|\+]+,-?[\d|.|e|E|\+]+,-?[\d|.|e|E|\+]+/g;
-	var start = 0;
-	for (let i = 0; i < placemark.length; i++) {
-		var jspace = placemark.indexOf(" ", i);
-		var substr = placemark.substring(i, jspace);
-		if (substr.match(regex) != null) {
-			start = i;
-			break;
-		}
-	}
+  var regex = /-?[\d|.|e|E|\+]+,-?[\d|.|e|E|\+]+,-?[\d|.|e|E|\+]+/g;
+  var locations = placemark
+    .match(regex)
+    .map(a => a.split(",")
+      .map(b => Number(b))
+      .slice(0,2))
+    .map(a => new Location(a[1], a[0]))
 
-	var end = 0;
-	for (let i = start; i < placemark.length; i++) {
-		var jspace = placemark.indexOf(" ", i);
-		var substr = placemark.substring(i, jspace);
-		if (substr.match(regex) == null) {
-			end = i;
-			break;
-		}
-		i += substr.length;
-	}
-
-	var coordinateStr = placemark.substring(start, end);
-	var locations = coordinateStr
-		.trim()
-		.split(" ")
-		.map(a => a.split(",")
-			.map(b => Number(b))
-			.slice(0,2))
-		.map(a => new Location(a[1], a[0]))
-
-	return locations;
+  return locations;
 }
 
 export default {
-	locationsFromPlacemark
+  locationsFromPlacemark
 }
