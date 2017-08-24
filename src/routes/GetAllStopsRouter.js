5d5f70a14b6fe2693f07e89659e8230e0fa066eb
Jma353 <jma353@cornell.edu>
2017-08-24 19:50:46 -0400

    Runs with appdev core module linked

--- src/routes/GetAllStopsRouter.js
+++ src/routes/GetAllStopsRouter.js
@@ -1,2 +1,20 @@
 // @flow
-import appdev from 'appdev';
+import { AppDevRouter } from 'appdev';
+import { Request } from 'express';
+import TCAT from '../TCAT';
+
+class GetAllStopsRouter extends AppDevRouter {
+  constructor () {
+    super('GET');
+  }
+
+  getPath (): string {
+    return '/stops/';
+  }
+
+  async content (req: Request) {
+    return TCAT.stops;
+  }
+}
+
+export default new GetAllStopsRouter().router;
