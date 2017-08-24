cc85157a72566d49c57eaf85032e278514d8eefe
Jma353 <jma353@cornell.edu>
2017-08-24 18:29:47 -0400

    Resolve lint errors

---
+++ src/routes/GetAllStopsRouter.js
@@ -0,0 +1,2 @@
+// @flow
+import appdev from 'appdev';
--- src/utils/KmlUtils.js
+++ src/utils/KmlUtils.js
@@ -1,15 +1,11 @@
 // @flow
 
-import Location from '../models/Location';
-import Stop from '../models/Stop';
-
-
-const rotatedArray = (arr: Array, count: number): Array => {
-  count -= arr.length * Math.floor(count / arr.length)
-  arr.push.apply(arr, arr.splice(0, count))
-  return arr
-}
+const rotatedArray = (arr: Array<any>, count: number): Array<any> => {
+  count -= arr.length * Math.floor(count / arr.length);
+  arr.push.apply(arr, arr.splice(0, count));
+  return arr;
+};
 
 export default {
   rotatedArray
-}
+};
