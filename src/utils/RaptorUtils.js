1af09aa297e36717e073a2bbf18548be4fb5b05f
Jma353 <jma353@cornell.edu>
2017-08-26 16:56:23 -0400

    Async await for walking paths

--- src/utils/RaptorUtils.js
+++ src/utils/RaptorUtils.js
@@ -34,51 +34,47 @@ const generateRaptorPaths = (startTime: number): Array<RaptorPath> => {
   return raptorPaths;
 };
 
-const walkingPaths = (
-  start: Stop,
-  end: Stop,
-  startTime: number
-): Promise<any> => {
+const walkingPaths =
+async (start: Stop, end: Stop, startTime: number): Promise<any> => {
   const coordinates = [
     start.location.toArray(),
     end.location.toArray()
   ].concat(TCAT.stops.map(stop => stop.location.toArray()));
 
   const options = {coordinates: coordinates};
-  return OSRM.table(options).then(response => {
-    const durations = response.durations.map(a => a.map(b => b * 2));
-    let raptorPaths = [];
-    const day = dayFromTimeInSecs(startTime);
-    const tcatNum = TCATConstants.WALKING_TCAT_NUMBER;
+  const response = await OSRM.table(options);
+  const durations = response.durations.map(a => a.map(b => b * 2));
+  let raptorPaths = [];
+  const day = dayFromTimeInSecs(startTime);
+  const tcatNum = TCATConstants.WALKING_TCAT_NUMBER;
 
-    for (let i = 0; i < TCAT.stops.length; i++) {
-      // From Start
-      const fromStartTravelTime = durations[0][i + 2];
-      const fromStartTimedStops = [
-        new TimedStop(start, startTime),
-        new TimedStop(TCAT.stops[i], startTime + fromStartTravelTime)
-      ];
-      raptorPaths.push(new RaptorPath(day, tcatNum, fromStartTimedStops));
-
-      // To End
-      const toEndTravelTime = durations[i + 2][1];
-      const toEndTimedStops = [
-        new TimedStop(TCAT.stops[i], TCATConstants.BASE_END_TIME),
-        new TimedStop(end, TCATConstants.BASE_END_TIME + toEndTravelTime)
-      ];
-      raptorPaths.push(new RaptorPath(day, tcatNum, toEndTimedStops));
-    }
-
-    // Route that features walking from start to end
-    const startToEndTime = durations[0][1];
-    const startToEndTimedStops = [
-      new TimedStop(start, TCATConstants.BASE_END_TIME),
-      new TimedStop(end, TCATConstants.BASE_END_TIME + startToEndTime)
+  for (let i = 0; i < TCAT.stops.length; i++) {
+    // From Start
+    const fromStartTravelTime = durations[0][i + 2];
+    const fromStartTimedStops = [
+      new TimedStop(start, startTime),
+      new TimedStop(TCAT.stops[i], startTime + fromStartTravelTime)
     ];
-    raptorPaths.push(new RaptorPath(day, tcatNum, startToEndTimedStops));
+    raptorPaths.push(new RaptorPath(day, tcatNum, fromStartTimedStops));
 
-    return Promise.resolve(raptorPaths);
-  });
+    // To End
+    const toEndTravelTime = durations[i + 2][1];
+    const toEndTimedStops = [
+      new TimedStop(TCAT.stops[i], TCATConstants.BASE_END_TIME),
+      new TimedStop(end, TCATConstants.BASE_END_TIME + toEndTravelTime)
+    ];
+    raptorPaths.push(new RaptorPath(day, tcatNum, toEndTimedStops));
+  }
+
+  // Route that features walking from start to end
+  const startToEndTime = durations[0][1];
+  const startToEndTimedStops = [
+    new TimedStop(start, TCATConstants.BASE_END_TIME),
+    new TimedStop(end, TCATConstants.BASE_END_TIME + startToEndTime)
+  ];
+  raptorPaths.push(new RaptorPath(day, tcatNum, startToEndTimedStops));
+
+  return raptorPaths;
 };
 
 export default {
