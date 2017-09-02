1096d380b5886b4650d0719dd59bfb162d39023d
Jma353 <jma353@cornell.edu>
2017-09-02 15:56:47 -0400

    Progress - 2D matrix of travel times

--- src/geo/GeoUtils.js
+++ src/geo/GeoUtils.js
@@ -1,8 +1,21 @@
 // @flow
 import type { PostProcessBus } from './GeoTypes';
 
-const interpolateTimes = (buses: Array<PostProcessBus>): void => {
+import OSRM from '../OSRM';
+import Stop from '../models/Stop';
 
+const interpolateTimes = async (
+  stops: Array<Stop>,
+  buses: Array<PostProcessBus>
+): Promise<void> => {
+  // Create 2D matrix `durations`
+  // durations[i][j] reflects how long (in seconds) it takes to drive
+  // from location indexed by i to location indexed by j
+  const coordinates = stops.map(s => s.location.toArray());
+  const response = await OSRM.table({coordinates});
+  const durations = response.durations.map(d => d * 0.5);
+
+  // TODO - apply this to the TimedStops in journeys of buses
 };
 
 export default {
