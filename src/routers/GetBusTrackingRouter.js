2b514b2e4170c7c1be2ce5714d43e77aef540a9d
Austin <astorgaaustin@gmail.com>
2017-09-02 14:34:35 -0400

    fixed endpoint to properly handle async/await and throw errors when necessary. Tested and it works

--- src/routers/GetBusTrackingRouter.js
+++ src/routers/GetBusTrackingRouter.js
@@ -15,45 +15,42 @@ class GetBusTrackingRouter extends AppDevRouter {
   async content (req: Request) {
     const routeID = req.query.routeID;
     const baseURL = 'https://realtimetcatbus.availtec.com/InfoPoint/rest/Vehicles/GetAllVehiclesForRoute';
-    axios.get(baseURL, {
-      params: {
-        routeID: routeID
-      }
-    })
-      .then((response) => {
-        var parsedData = response.data.map((busInfo) => {
-          return {
-            commStatus: busInfo.CommStatus,
-            destination: busInfo.Destination,
-            deviation: busInfo.Deviation,
-            direction: busInfo.Direction,
-            displayStatus: busInfo.DisplayStatus,
-            gpsStatus: busInfo.GPSStatus,
-            heading: busInfo.Heading,
-            lastStop: busInfo.LastStop,
-            lastUpdated: busInfo.LastUpdated,
-            latitude: busInfo.Latitude,
-            longitude: busInfo.Longitude,
-            name: busInfo.Name,
-            opStatus: busInfo.OpStatus,
-            routeID: busInfo.RouteId,
-            runID: busInfo.RunId,
-            speed: busInfo.Speed,
-            tripID: busInfo.TripId,
-            behicleID: busInfo.VehicleId
-          };
-        });
+    try {
+      const response = await axios.get(baseURL, {
+        params: {
+          routeID: routeID
+        }
+      });
+      const parsedData = response.data.map((busInfo) => {
+        var lastUpdated = busInfo.LastUpdated;
+        const firstParan = lastUpdated.indexOf('(') + 1;
+        const secondParan = lastUpdated.indexOf('-');
+        lastUpdated = parseInt(lastUpdated.slice(firstParan, secondParan));
         return {
-          success: true,
-          data: parsedData
-        };
-      })
-      .catch(() => {
-        return {
-          success: false,
-          data: []
+          commStatus: busInfo.CommStatus,
+          destination: busInfo.Destination,
+          deviation: busInfo.Deviation,
+          direction: busInfo.Direction,
+          displayStatus: busInfo.DisplayStatus,
+          gpsStatus: busInfo.GPSStatus,
+          heading: busInfo.Heading,
+          lastStop: busInfo.LastStop,
+          lastUpdated: lastUpdated,
+          latitude: busInfo.Latitude,
+          longitude: busInfo.Longitude,
+          name: busInfo.Name,
+          opStatus: busInfo.OpStatus,
+          routeID: busInfo.RouteId,
+          runID: busInfo.RunId,
+          speed: busInfo.Speed,
+          tripID: busInfo.TripId,
+          vehicleID: busInfo.VehicleId
         };
       });
+      return parsedData;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
 
