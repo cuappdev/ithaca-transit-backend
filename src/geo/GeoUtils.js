7f0a9bda97f86bf9425821c4c0c92032e18429ed
Jma353 <jma353@cornell.edu>
2017-09-03 02:44:25 -0400

    Remove dependency on GTFS + change timepoint to isTimepoint

--- src/geo/GeoUtils.js
+++ src/geo/GeoUtils.js
@@ -1,23 +1,26 @@
 // @flow
 import type { PostProcessJourney, PostProcessBus } from './GeoTypes';
 
-import GTFS from '../GTFS';
 import OSRM from '../OSRM';
+import Stop from '../models/Stop';
 import TimedStop from '../models/TimedStop';
 
-const interpolateTimes =
-async (buses: Array<PostProcessBus>): Promise<void> => {
+const interpolateTimes = async (
+  buses: Array<PostProcessBus>,
+  stops: Array<Stop>,
+  nameToStopIndex: { [string]: [number] }
+): Promise<void> => {
   // Create 2D matrix `durations`
   // durations[i][j] reflects how long (in seconds) it takes to drive
   // from location indexed by i to location indexed by j
-  const coordinates = GTFS.stops.map(s => s.location.toArray());
+  const coordinates = stops.map(s => s.location.toArray());
   const response = await OSRM.table({coordinates});
   const durations = response.durations.map(d => d * 0.5);
 
   // first -> second TimedStop (the time to travel)
   const getTimeDiff = (first: TimedStop, second: TimedStop): number => {
-    const i = GTFS.nameToStopIndex[first.stop.name];
-    const j = GTFS.nameToStopIndex[second.stop.name];
+    const i = nameToStopIndex[first.stop.name];
+    const j = nameToStopIndex[second.stop.name];
     return durations[i][j];
   };
 
@@ -28,7 +31,7 @@ async (buses: Array<PostProcessBus>): Promise<void> => {
     // or else we have no frame of reference as to when this journey occurs
     let firstTimepointIdx = journey.stops.length;
     for (let i = 0; i < journey.stops.length; i++) {
-      if (journey.stops[i].timepoint) {
+      if (journey.stops[i].isTimepoint) {
         firstTimepointIdx = i;
         break;
       }
@@ -46,11 +49,11 @@ async (buses: Array<PostProcessBus>): Promise<void> => {
 
     // Forwards stage
     for (let k = firstTimepointIdx + 1; k < journey.stops.length; k++) {
-      if (journey.stops[k].timepoint) continue; // if it is already timepoint
+      if (journey.stops[k].isTimepoint) continue; // if it is already timepoint
       let timedStop = journey.stops[k];
       let prevTimedStop = journey.stops[k - 1];
-      const i = GTFS.nameToStopIndex[timedStop.stop.name];
-      const j = GTFS.nameToStopIndex[prevTimedStop.stop.name];
+      const i = nameToStopIndex[timedStop.stop.name];
+      const j = nameToStopIndex[prevTimedStop.stop.name];
       let timeDiff = durations[i][j];
       timedStop.time = prevTimedStop.time + timeDiff;
     }
