62399ab2fbd756488a444df911fd44aaa26163e2
Sanjana Kaundinya <skaundinya@gmail.com>
2017-09-07 01:27:38 -0400

    final changes implemented

--- src/routers/GetRouteRouter.js
+++ src/routers/GetRouteRouter.js
@@ -96,25 +96,35 @@ class GetRouteRouter extends AppDevRouter {
     // Construct a stopSummary array of JSON objects with specific details for
     // each stop in the route
     const stopSummary = [];
-    for (let i = 0; i < result.length; i++) {
-      const currResult = result[i];
+    const currLocation = {};
+    currLocation.name = TCATConstants.LOCATION_NAME_CURR;
+    currLocation.busNumber = mainStopNums[0];
+    currLocation.nextDirection = TCATConstants.NEXT_DIRECTION_WALK;
+    currLocation.type = TCATConstants.LOCATION_TYPE_CURR;
+    stopSummary.push(currLocation);
+    for (let i = 1; i <= result.length; i++) {
       const currNode = {};
-      if (i === 0) {
-        currNode.name = TCATConstants.LOCATION_NAME_CURR;
-        currNode.nextDirection = TCATConstants.NEXT_DIRECTION_WALK;
-        currNode.type = TCATConstants.LOCATION_TYPE_CURR;
-      } else {
-        currNode.name = mainStops[i-1];
+      // the following if block is if the final destination is a bus stop and
+      // an end location needs to be added to the array of json objects
+      if ((result.length === mainStopNums.length) && (i === result.length)) {
+        currNode.name = TCATConstants.LOCATION_NAME_END;
+        currNode.nextDirection = TCATConstants.NEXT_DIRECTION_NONE;
+        currNode.type = TCATConstants.LOCATION_TYPE_PLACE;
+        stopSummary.push(currNode);
+        break;
       }
+      const currResult = result[i];
+      currNode.name = mainStops[i-1];
       currNode.busNumber = mainStopNums[i];
       if (currNode.busNumber === -1) {
+
         currNode.nextDirection = TCATConstants.NEXT_DIRECTION_WALK;
       } else {
         currNode.nextDirection = TCATConstants.NEXT_DIRECTION_BUS;
       }
-      if (i === result.length - 1) {
+      if (result.length === mainStops.length){
         currNode.type = TCATConstants.LOCATION_TYPE_PLACE;
-      } else if (!currNode.type) {
+      } else {
         currNode.type = TCATConstants.LOCATION_TYPE_STOP;
       }
       stopSummary.push(currNode);
