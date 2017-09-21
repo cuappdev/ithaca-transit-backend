037d238b8bb1a47af257ccba33fca1a254e473b5
Jma353 <jma353@cornell.edu>
2017-09-21 12:23:12 -0400

    Clean up test-cases + add first test-case

--- src/__tests__/BasedRaptor.test.js
+++ src/__tests__/BasedRaptor.test.js
@@ -1,37 +1,23 @@
 /* eslint-disable no-undef */
 import 'babel-polyfill';
-
-import BasedRaptor from '../BasedRaptor';
-import BasedRaptorUtils from '../utils/BasedRaptorUtils';
-import Location from '../models/Location';
-import Stop from '../models/Stop';
 import TestUtils from '../utils/TestUtils';
 
-const raptorInstanceGenerator = async (path: string): BasedRaptor => {
-  const testJson = require(path);
-  const raptorInput = TestUtils.generateDataStructures(testJson);
-  const footpathMatrix =
-    await BasedRaptorUtils.footpathMatrix(raptorInput.start, raptorInput.end);
-  return new BasedRaptor(
-    raptorInput.buses,
-    raptorInput.start,
-    raptorInput.end,
-    raptorInput.stopsToRoutes,
-    footpathMatrix,
-    raptorInput.startTime,
-    raptorInput.stops
-  );
-}
+import path from 'path';
 
 describe('Raptor Test', () => {
   it('Basic Test', async () => {
-    const rapt = await raptorInstanceGenerator('../__tests__/test_data/first_test.json');
+    const rapt =
+      await TestUtils.raptorInstanceGenerator(
+        path.join(__dirname, './data/first_test.json')
+      );
     const result = await rapt.run();
-    console.log(result);
-  });
-  it('Basic Test', async () => {
-    const rapt = await raptorInstanceGenerator("../__tests__/test_data/second_test.json");
-    const result = await rapt.run();
-    console.log(result);
+    expect(result[0].arrivalTime === 150); // 0 -> 10 -> 100 -> 150
+    expect(result[1].arrivalTime === 320); // 0 -> 320
+
+    // Peep the first response
+    expect(result[0].path[0].start.name).toEqual('Start');
+    expect(result[0].path[0].end.name).toEqual('0');
+    expect(result[0].path[1].start.name).toEqual('0');
+    expect(result[0].path[1].end.name).toEqual('1');
   });
 });
