cd301f9f3e643b44a3875e961f795f2875c5874b
Robert Barrett <rpb83@cornell.edu>
2017-09-21 04:15:42 -0400

    modified testing framework to take in json input, json naming/import might need work (?)

--- src/__tests__/BasedRaptor.test.js
+++ src/__tests__/BasedRaptor.test.js
@@ -7,89 +7,30 @@ import Location from '../models/Location';
 import Stop from '../models/Stop';
 import TestUtils from '../utils/TestUtils';
 
+const raptorInstanceGenerator = async (path: string): BasedRaptor => {
+  const testJson = require(path);
+  const raptorInput = TestUtils.generateDataStructures(testJson);
+  const footpathMatrix =
+    await BasedRaptorUtils.footpathMatrix(raptorInput.start, raptorInput.end);
+  return new BasedRaptor(
+    raptorInput.buses,
+    raptorInput.start,
+    raptorInput.end,
+    raptorInput.stopsToRoutes,
+    footpathMatrix,
+    raptorInput.startTime,
+    raptorInput.stops
+  );
+}
+
 describe('Raptor Test', () => {
   it('Basic Test', async () => {
-    const testCase = {
-      stops: [
-        {
-          name: 'Schwartz Performing Arts Center',
-          lat: 42.442558,
-          long: -76.485336
-        },
-        {
-          name: 'Carpenter Hall',
-          lat: 42.444889,
-          long: -76.484993
-        }
-      ],
-      buses: [{
-        lineNumber: 10,
-        paths: [[{stopIndex: 0, time: 1000}, {stopIndex: 1, time: 2000}]]
-      }]
-    };
-
-    // Generate data-structures from testCase JSON
-    const raptorInput = TestUtils.generateDataStructures(testCase);
-    // CTB
-    const start = new Stop('Start', new Location(42.442579, -76.485068));
-    // Statler
-    const end = new Stop('End', new Location(42.445627, -76.482600));
-    // Footpath transitions
-    const footpathMatrix = await BasedRaptorUtils.footpathMatrix(start, end);
-    // Raptor instance
-    const rapt = new BasedRaptor(
-      raptorInput.buses,
-      start,
-      end,
-      raptorInput.stopsToRoutes,
-      footpathMatrix,
-      900,
-      raptorInput.stops
-    );
-
-    // Run + check
+    const rapt = await raptorInstanceGenerator('../__tests__/test_data/first_test.json');
     const result = await rapt.run();
+    console.log(result);
   });
   it('Basic Test', async () => {
-    const testCase = {
-      stops: [
-        {
-          name: 'Schwartz Performing Arts Center',
-          lat: 42.442558,
-          long: -76.485336
-        },
-        {
-          name: 'Carpenter Hall',
-          lat: 42.444889,
-          long: -76.484993
-        }
-      ],
-      buses: [{
-        lineNumber: 10,
-        paths: [[{stopIndex: 0, time: 800}, {stopIndex: 1, time: 1110}]]
-      }]
-    };
-
-    // Generate data-structures from testCase JSON
-    const raptorInput = TestUtils.generateDataStructures(testCase);
-    // CTB
-    const start = new Stop('Start', new Location(42.442558, -76.485336));
-    // Statler
-    const end = new Stop('End', new Location(42.444889, -76.484993));
-    // Footpath transitions
-    const footpathMatrix = await BasedRaptorUtils.footpathMatrix(start, end);
-    // Raptor instance
-    const rapt = new BasedRaptor(
-      raptorInput.buses,
-      start,
-      end,
-      raptorInput.stopsToRoutes,
-      footpathMatrix,
-      800,
-      raptorInput.stops
-    );
-
-    // Run + check
+    const rapt = await raptorInstanceGenerator("../__tests__/test_data/second_test.json");
     const result = await rapt.run();
     console.log(result);
   });
