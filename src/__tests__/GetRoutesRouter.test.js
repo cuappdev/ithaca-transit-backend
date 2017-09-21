c4ce43f42dc689c7c4f319573c19ba5309c7109f
Robert Barrett <rpb83@cornell.edu>
2017-09-21 01:06:54 -0400

    modified error-throwing but correct test to cater to joe's sensibilities

--- src/__tests__/GetRoutesRouter.test.js
+++ src/__tests__/GetRoutesRouter.test.js
@@ -6,7 +6,7 @@ const app = new API().express;
 
 describe('GetAllStopsRouter Success Test', () => {
   it('Assert response body contains success and data', () => {
-    return request(app).get('/api/v1/routes/')
+    return request(app).get('/api/v1/routes?leave_by=1504474540&start_coords=42.4424,-76.4849&end_coords=42.483327,-76.490933')
       .expect(200)
       .then((res) => {
         const reqKeys = ['success', 'data'];
