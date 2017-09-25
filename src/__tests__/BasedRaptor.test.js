3d6b1df008735e52c83bc25cd2643fa470667c6f
Robert Barrett <rpb83@cornell.edu>
2017-09-25 01:40:58 -0400

    Testing a later but faster bus

--- src/__tests__/BasedRaptor.test.js
+++ src/__tests__/BasedRaptor.test.js
@@ -26,6 +26,7 @@ describe('Raptor Test', () => {
   it('Basic 2', async () => {
     const rapt = await TestUtils.raptorInstanceGenerator(fl('./data/2.json'));
     const result = await rapt.run();
+    console.log(result);
     expect(result[0].arrivalTime).toEqual(140);
     expect(result[1].arrivalTime).toEqual(260);
 
@@ -40,6 +41,7 @@ describe('Raptor Test', () => {
     const rapt = await TestUtils.raptorInstanceGenerator(fl('./data/3.json'));
     const result = await rapt.run();
     expect(result[0].arrivalTime).toEqual(240);
+    console.log(result[0].path);
 
     // Peep the optimal path
     expect(result[0].path[0].start.name).toEqual('Start');
@@ -50,5 +52,17 @@ describe('Raptor Test', () => {
     expect(result[0].path[2].end.name).toEqual('2');
     expect(result[0].path[3].start.name).toEqual('2');
     expect(result[0].path[3].end.name).toEqual('3');
+  });
+
+  it('Prioritize Later But Faster Bus 1', async () => {
+    const rapt = await TestUtils.raptorInstanceGenerator(fl('./data/4.json'));
+    const result = await rapt.run();
+
+    // Optimal path
+    expect(result[0].arrivalTime).toEqual(200);
+    expect(result[0].path[0].start.name).toEqual('Start');
+    expect(result[0].path[0].end.name).toEqual('0');
+    expect(result[0].path[1].start.name).toEqual('0');
+    expect(result[0].path[1].end.name).toEqual('3');
   });
 });
