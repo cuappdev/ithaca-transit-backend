5bd557ba368021ed0f7897bf02c15c35dad2a315
Jma353 <jma353@cornell.edu>
2017-10-06 22:17:39 -0400

    Some progress

--- src/__tests__/GTFS.test.js
+++ src/__tests__/GTFS.test.js
@@ -10,10 +10,10 @@ describe('Testing GTFS', () => {
   it('Times in paths are monotonically nondecreasing', async () => {
     const busData = await GTFS.buses(serviceDate);
     const busArray = busData.buses;
-    for (var key in busArray) {
-      var bus = busArray[key];
-      for (var path in bus.paths) {
-        for (var timedStops in path.timedStops) {
+    for (let key in busArray) {
+      let bus = busArray[key];
+      for (let path in bus.paths) {
+        for (let timedStops in path.timedStops) {
           for (let i = 1; i < timedStops.length; i++) {
             expect(timedStops[i].time >= timedStops[i - 1].time);
           }
