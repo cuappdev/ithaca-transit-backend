7f31cfa565d08cb8a17170d1146765122a819c5c
mattbarker016 <mjb485@cornell.edu>
2017-10-15 01:59:07 -0400

    Added Basic Transfer Test

--- src/__tests__/BasedRaptor.test.js
+++ src/__tests__/BasedRaptor.test.js
@@ -26,10 +26,6 @@ describe('Raptor Test', () => {
   it('Basic 2', async () => {
     const rapt = await TestUtils.raptorInstanceGenerator(fl('./data/2.json'));
     const result = await rapt.run();
-<<<<<<< HEAD
-    console.log(result);
-=======
->>>>>>> f1755b666e0649c8b17073c85eec8614bdda524d
     expect(result[0].arrivalTime).toEqual(140);
     expect(result[1].arrivalTime).toEqual(260);
 
@@ -41,43 +37,15 @@ describe('Raptor Test', () => {
   });
 
   it('Basic 3', async () => {
-<<<<<<< HEAD
-    const rapt = await TestUtils.raptorInstanceGenerator(fl('./data/5.json'));
-    const result = await rapt.run();
-    console.log(result);
-    expect(result[0].arrivalTime).toEqual(290);
-    expect(result[1].arrivalTime).toEqual(360);
-=======
     const rapt = await TestUtils.raptorInstanceGenerator(fl('./data/3.json'));
     const result = await rapt.run();
     expect(result[0].arrivalTime).toEqual(220);
     expect(result[1].arrivalTime).toEqual(225);
->>>>>>> f1755b666e0649c8b17073c85eec8614bdda524d
 
     // Peep the optimal path
     expect(result[0].path[0].start.name).toEqual('Start');
     expect(result[0].path[0].end.name).toEqual('0');
     expect(result[0].path[1].start.name).toEqual('0');
-<<<<<<< HEAD
-    expect(result[0].path[1].end.name).toEqual('2');
-  });
-
-  it('Basic Walking Transfer 1', async () => {
-    const rapt = await TestUtils.raptorInstanceGenerator(fl('./data/3.json'));
-    const result = await rapt.run();
-    expect(result[0].arrivalTime).toEqual(240);
-    console.log(result[0].path);
-
-    // Peep the optimal path
-    expect(result[0].path[0].start.name).toEqual('Start');
-    expect(result[0].path[0].end.name).toEqual('0');
-    expect(result[0].path[1].start.name).toEqual('0');
-    expect(result[0].path[1].end.name).toEqual('1');
-    expect(result[0].path[2].start.name).toEqual('1');
-    expect(result[0].path[2].end.name).toEqual('2');
-    expect(result[0].path[3].start.name).toEqual('2');
-    expect(result[0].path[3].end.name).toEqual('3');
-=======
     expect(result[0].path[1].end.name).toEqual('3');
   });
 
@@ -91,20 +59,41 @@ describe('Raptor Test', () => {
     expect(result[0].path[0].end.name).toEqual('2');
     expect(result[0].path[1].start.name).toEqual('2');
     expect(result[0].path[1].end.name).toEqual('3');
->>>>>>> f1755b666e0649c8b17073c85eec8614bdda524d
   });
 
   it('Walking is fastest path', async () => {
     const rapt = await TestUtils.raptorInstanceGenerator(fl('./data/6.json'));
     const result = await rapt.run();
 
-    // TODO: Optimal path
+    // Peep the optimal path
     expect(result[0].arrivalTime).toEqual(1000);
     expect(result[0].path[0].start.name).toEqual('Start');
     expect(result[0].path[0].end.name).toEqual('End');
   });
-<<<<<<< HEAD
-  
-=======
->>>>>>> f1755b666e0649c8b17073c85eec8614bdda524d
+
+  it('Basic transfer', async () => {
+    const rapt = await TestUtils.raptorInstanceGenerator(fl('./data/7.json'));
+    const result = await rapt.run();
+
+    expect(result[0].arrivalTime).toEqual(450);
+    // Walk to Bus Stop 0
+    expect(result[0].path[0].start.name).toEqual('Start');
+    expect(result[0].path[0].end.name).toEqual('0');
+    // Bus ending at Bus Stop 1
+    expect(result[0].path[1].start.name).toEqual('0');
+    expect(result[0].path[1].end.name).toEqual('1');
+    // Walk from Bus Stop 1 to Bus Stop 3
+    expect(result[0].path[2].start.name).toEqual('1');
+    expect(result[0].path[2].end.name).toEqual('3');
+    // Bus ending at Bus Stop 4
+    expect(result[0].path[3].start.name).toEqual('3');
+    expect(result[0].path[3].end.name).toEqual('4');
+    // Walk to End
+    expect(result[0].path[4].start.name).toEqual('4');
+    expect(result[0].path[4].end.name).toEqual('End');
+
+    // TODO: Check other reccomended paths?
+
+  });
+
 });
