839c9f664074d751a97a4c3f8559cd9d0c809877
vagrant <vagrant@vagrant.vm>
2018-02-07 05:13:44 +0000

    Complete Web API port

--- src/API.js
+++ src/API.js
@@ -29,13 +29,16 @@ class API {
 
   routes (): void {
     this._use(HelloWorldRouter);
-    this._use(GetAllStopsRouter);
-    this._use(GetRouteRouter);
-    this._use(GetBusesRouter);
-    this._use(GetBusTrackingRouter);
-    this._use(new WebApiCallRouter('/rest/RouteDetails/GetAllRouteDetails', '/routeDetails/').router);
-//    this._use(new WebApiCallRouter('/rest/Stop/GetAllStops').router);
-//    this._use(new WebApiCallRouter('/rest/StopDepartures/GetAllStopDepartures').router);
+    //this._use(GetAllStopsRouter);
+    //this._use(GetRouteRouter);
+    //this._use(GetBusesRouter);
+    //this._use(GetBusTrackingRouter);
+    this._use(new WebApiCallRouter('/rest/RouteDetails/GetAllRouteDetails', '/webApi/routeDetails/').router);
+    this._use(new WebApiCallRouter('/rest/Stop/GetAllStops', '/webApi/stops/').router);
+    this._use(new WebApiCallRouter('/rest/StopDepartures/GetAllStopDepartures', '/webApi/stopDepartures/').router);
+    this._use(new WebApiCallRouter('/rest/Vehicles/GetAllVehicles', '/webApi/vehicles/').router);
+    this._use(new WebApiCallRouter('/rest/TransitAuthorityConfig/Get', '/webApi/transitAuthorityConfig/').router);
+    this._use(new WebApiCallRouter('/rest/PublicMessages/GetAllMessages', '/webApi/publicMessages/').router);
   }
 }
 export default API;
