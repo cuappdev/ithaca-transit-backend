e3ed5545de4dcb6e1e796dd1fe70176c43fd1eb6
vagrant <vagrant@vagrant.vm>
2018-02-21 10:25:29 +0000

    Barebones backend

--- src/models/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-export { default as Bus } from './Bus';
-export { default as BusPath } from './BusPath';
-export { default as FootpathMatrix } from './FootpathMatrix';
-export { default as Kml } from './Kml';
-export { default as Location } from './Location';
-export { default as Path } from './Path';
-export { default as Stop } from './Stop';
-export { default as TimedStop } from './TimedStop';
