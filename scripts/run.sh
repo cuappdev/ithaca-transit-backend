04ab576a38ef3c42f53197bb1fb0a4a548b39dd1
vagrant <vagrant@vagrant.vm>
2018-02-21 17:02:11 +0000

    Commit graph

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,4 @@
 java -Xmx1g -Xms1g -jar graphhopper/web/target/graphhopper-web-*-with-dep.jar datareader.file=osrm/map.osm gtfs.file=tcat-ny-us.zip jetty.port=8989 jetty.resourcebase=./graphhopper/web/src/main/webapp graph.flag_encoders=pt prepare.ch.weightings=no graph.location=./graph-cache &>/dev/null &
+sleep 3s
 npm run serve
-killall -9 java
+
