0f021747af136997ae90d7276d0a650e024d17f1
Austin <astorgaaustin@gmail.com>
2018-02-25 12:16:17 -0500

    fixed issues with allStops and tracking because of async call not handled properly.

--- src/AllStopsRouter.js
+++ src/AllStopsRouter.js
@@ -5,26 +5,22 @@ import axios from 'axios';
 class AllStopsRouter extends AbstractRouter {
 
     constructor() {
-        super('GET', '/allStops/', false);
+        super('GET', '/allStops', false);
     }
 
     async content(req: Request): Promise<any> {
         const AuthStr = 'Bearer 5a54bc7f-a7df-3796-a83a-5bba7a8e31c8'; // Accept: "application/json"
         try {
-            axios.get('https://gateway.api.cloud.wso2.com:443/t/mystop/tcat/v1/rest/Stops/GetAllStops',
-                {headers: {Authorization: AuthStr}}).then(response => {
-                let allStops = [];
-                let tcatAllStops = response.data;
-                for (let i = 0; i < tcatAllStops.length; i++) {
-                    let stopData = tcatAllStops[i];
-                    allStops.push({
-                        name: stopData.Name,
-                        lat: stopData.Latitude,
-                        lon: stopData.Longitude
-                    });
+            let stopsRequest = await axios.get('https://gateway.api.cloud.wso2.com:443/t/mystop/tcat/v1/rest/Stops/GetAllStops',
+                {headers: {Authorization: AuthStr}});
+            let tcatAllStops = stopsRequest.data.map(stop => {
+                return {
+                    name: stop.Name,
+                    lat: stop.Latitude,
+                    long: stop.Longitude
                 }
-                return JSON.stringify(allStops);
-            })
+            });
+            return JSON.stringify(tcatAllStops);
         } catch (error) {
             return 'error ** ' + error;
         }
--- src/TrackingRouter.js
+++ src/TrackingRouter.js
@@ -5,17 +5,45 @@ import axios from 'axios';
 class TrackingRouter extends AbstractRouter {
 
     constructor() {
-        super('GET', '/tracking/', false);
+        super('GET', '/tracking', false);
     }
 
     async content(req: Request): Promise<any> {
         let routeID = req.query.routeID;
         const AuthStr = 'Bearer 5a54bc7f-a7df-3796-a83a-5bba7a8e31c8';
-        axios.get('https://realtimetcatbus.availtec.com/InfoPoint/rest/Vehicles/GetAllVehiclesForRoute?routeID=' + routeID, {headers: {Authorization: AuthStr}}).then(response => {
-            return response.data;
-        }).catch((error) => {
-            return 'error ** ' + error;
-        });
+        try {
+        let trackingRequest = await axios.get('https://realtimetcatbus.availtec.com/InfoPoint/rest/Vehicles/GetAllVehiclesForRoute?routeID=' + routeID, {headers: {Authorization: AuthStr}});
+        const trackingData = trackingRequest.data.map((busInfo) => {
+            var lastUpdated = busInfo.LastUpdated;
+            const firstParan = lastUpdated.indexOf('(') + 1;
+            const secondParan = lastUpdated.indexOf('-');
+            lastUpdated = parseInt(lastUpdated.slice(firstParan, secondParan));
+            return {
+              commStatus: busInfo.CommStatus,
+              destination: busInfo.Destination,
+              deviation: busInfo.Deviation,
+              direction: busInfo.Direction,
+              displayStatus: busInfo.DisplayStatus,
+              gpsStatus: busInfo.GPSStatus,
+              heading: busInfo.Heading,
+              lastStop: busInfo.LastStop,
+              lastUpdated: lastUpdated,
+              latitude: busInfo.Latitude,
+              longitude: busInfo.Longitude,
+              name: busInfo.Name,
+              opStatus: busInfo.OpStatus,
+              routeID: busInfo.RouteId,
+              runID: busInfo.RunId,
+              speed: busInfo.Speed,
+              tripID: busInfo.TripId,
+              vehicleID: busInfo.VehicleId
+            };
+          });
+          return JSON.stringify(trackingData);
+        } catch (error) {
+            console.log(error);
+            return 'error';
+        }
     }
 
 }
