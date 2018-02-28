93714a66b66e263c4c0bbf8dd5aeee2998e863fa
Austin <astorgaaustin@gmail.com>
2018-02-27 22:05:21 -0500

    prepped for PR

--- scripts/graph.sh
+++ scripts/graph.sh
@@ -1,7 +1,3 @@
 rm -rf graph-cache
 java -Xmx1g -Xms1g -jar graphhopper/web/target/graphhopper-web-*-with-dep.jar datareader.file=osrm/map.osm gtfs.file=tcat-ny-us.zip jetty.port=8988 jetty.resourcebase=./graphhopper/web/src/main/webapp graph.flag_encoders=pt prepare.ch.weightings=no graph.location=./graph-cache
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a7af952be41912217a052f784fc4b363305b94f9
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,17 +3,11 @@ git clone https://github.com/graphhopper/graphhopper.git
 git clone https://github.com/graphhopper/map-matching.git
 cd map-matching
 git checkout 0.9
-<<<<<<< HEAD
 ./map-matching.sh action=import datasource=../osrm/map.osm vehicle=car,foot
 cd ..
 wget https://s3.amazonaws.com/tcat-gtfs/tcat-ny-us.zip
 mkdir tcat-ny-us
 tar xvf tcat-ny-us.zip -C tcat-ny-us
-=======
-./map-matching.sh action=import datasource=../osrm/tcat.osm.pbf vehicle=car,foot
-cd ..
-wget https://s3.amazonaws.com/tcat-gtfs/tcat-ny-us.zip
->>>>>>> a7af952be41912217a052f784fc4b363305b94f9
 cd graphhopper
 ./graphhopper.sh buildweb
 cd ..
