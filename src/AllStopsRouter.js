678790aca7726cbce3de650fefea032463bb9dcf
Arman <arman.esmaili1@gmail.com>
2018-03-02 17:21:09 -0500

    Abstracts token generation into TokenUtils

--- src/AllStopsRouter.js
+++ src/AllStopsRouter.js
@@ -1,4 +1,5 @@
 // @flow
+import TokenUtils from './TokenUtils';
 import AbstractRouter from './AbstractRouter';
 import axios from 'axios';
 import qs from 'qs';
@@ -10,52 +11,31 @@ class AllStopsRouter extends AbstractRouter {
     }
 
     async content(req: Request): Promise<any> {
-    	let newTokenAttempts = 0;
-    	var newTokenError = null;
-    	do {
-			let credentials = require("../config.json");
-			let apiAuth = 'Bearer ' + credentials.access_token;
-			try {
-				let stopsRequest = await axios.get('https://gateway.api.cloud.wso2.com:443/t/mystop/tcat/v1/rest/Stops/GetAllStops',
-					{headers: {Authorization: apiAuth}});
-				let allStopsTcat = stopsRequest.data.map(stop => {
-					return {
-						name: stop.Name,
-						lat: stop.Latitude,
-						long: stop.Longitude
-					}
-				});
-				return JSON.stringify(allStopsTcat);
-			} catch (error) {
-				let errorCode = error.response.status;
-				if (errorCode == 401) { // current access token is expired
-					newTokenAttempts += 1;
-					const tokenAuth = 'Basic ' + credentials.basic_token;
-					const https = require('https');
-					const agent = new https.Agent({  
-					  rejectUnauthorized: false
-					});
-					try {
-	 		            let tokenRequest = await axios.post('https://gateway.api.cloud.wso2.com:443/token', qs.stringify({"grant_type": "client_credentials"}), {
-							httpsAgent: agent, 
-							headers: {Authorization: tokenAuth}
-	 		            });
-						const fs = require('fs');
-						const newCredentials = {basic_token: credentials.basic_token, 
-												access_token: tokenRequest.data.access_token};
-						fs.writeFile('config.json', JSON.stringify(newCredentials), 'utf8', function(err) {
-							if (err)
-								console.log(err);
-						});
-					} catch (error) {
-						newTokenError = error;
-					}
-				} else { // unexpected error
-					return 'error ** ' + error;
-				}
+    	try {
+    		// generate or retrieve TCAT API access token
+			let isAccessTokenExpired = await TokenUtils.isAccessTokenExpired();
+			let accessToken;
+			if (isAccessTokenExpired) {
+				accessToken = await TokenUtils.generateAccessToken();
+			} else {
+				accessToken = await TokenUtils.getAccessToken();
 			}
-		} while (newTokenAttempts < 4);
-		return 'error generating new token **' + newTokenError;
+			
+			// make request to TCAT API
+			let apiAuth = 'Bearer ' + accessToken;
+			let stopsRequest = await axios.get('https://gateway.api.cloud.wso2.com:443/t/mystop/tcat/v1/rest/Stops/GetAllStops',
+				{headers: {Authorization: apiAuth}});
+			let allTcatStops = stopsRequest.data.map(stop => {
+				return {
+					name: stop.Name,
+					lat: stop.Latitude,
+					long: stop.Longitude
+				}
+			});
+			return JSON.stringify(allTcatStops);
+		} catch (err) {
+			throw err;
+		}
     }
 }
 
---
+++ src/TokenUtils.js
@@ -0,0 +1,70 @@
+// @flow
+import fs from 'fs';
+import https from 'https';
+import axios from 'axios';
+import qs from 'qs';
+
+function getCredentials() {
+	return require('../config.json');
+}
+
+async function isAccessTokenExpired() {
+    return new Promise(async function (resolve, reject) {
+    	try {
+			let credentials = getCredentials();
+			let currentDate = new Date();
+			let expiryDate = new Date(credentials.expiry_date);
+			let isTokenExpired = expiryDate.getTime() < currentDate.getTime();
+			resolve(isTokenExpired);
+        } catch (err) {
+        	reject(err);
+        }
+    });
+}
+
+async function generateAccessToken() {
+	return new Promise(async function (resolve, reject) {
+		try {
+			let credentials = getCredentials();
+			const tokenAuth = 'Basic ' + credentials.basic_token;
+			const agent = new https.Agent({  
+			  rejectUnauthorized: false
+			});
+			let tokenRequest = await axios.post('https://gateway.api.cloud.wso2.com:443/token', qs.stringify({"grant_type": "client_credentials"}), {
+				httpsAgent: agent, 
+				headers: {Authorization: tokenAuth}
+			});
+			let currentDate = new Date();
+			let newCredentials = {basic_token: credentials.basic_token, 
+								  access_token: tokenRequest.data.access_token,
+								  expiry_date: new Date(currentDate.getTime() + tokenRequest.data.expires_in*1000)};
+			fs.writeFile('config.json', JSON.stringify(newCredentials), 'utf8', function(err) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(newCredentials.access_token);
+				}
+			});
+		} catch (err) {
+			reject(err);
+		}
+	});
+
+}
+
+async function getAccessToken() {
+	return new Promise(async function (resolve, reject) {
+    	try {
+			let credentials = getCredentials();
+			resolve(credentials.access_token);
+        } catch (err) {
+        	reject(err);
+        }
+	});
+}
+
+export default {
+    isAccessTokenExpired: isAccessTokenExpired,
+    generateAccessToken: generateAccessToken,
+    getAccessToken: getAccessToken
+}
