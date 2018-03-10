acada5f71209bc1f749092e54e96cd75e1827e05
Austin <astorgaaustin@gmail.com>
2018-03-10 13:11:19 -0500

    modified scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-java -Xmx1g -Xms1g -jar graphhopper/web/target/graphhopper-web-*-with-dep.jar datareader.file=osrm/map.osm gtfs.file=tcat-ny-us.zip jetty.port=8988 jetty.resourcebase=./graphhopper/web/src/main/webapp graph.flag_encoders=pt prepare.ch.weightings=no graph.location=./graph-cache &>/dev/null &
+java -Xmx1g -Xms1g -jar graphhopper/web/target/graphhopper-web-*-with-dep.jar datareader.file=osrm/map.osm gtfs.file=tcat-ny-us.zip jetty.port=8988 jetty.resourcebase=./graphhopper/web/src/main/webapp graph.flag_encoders=pt,foot prepare.ch.weightings=no graph.location=./graph-cache &>/dev/null &
 
 cd map-matching
 ./map-matching.sh action=start-server &>/dev/null &
