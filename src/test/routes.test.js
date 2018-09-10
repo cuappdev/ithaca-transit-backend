2c68cc21c973d253890493ecdfb913d33878e2bc
Nate <njs59@cornell.edu>
2018-09-09 22:12:23 -0400

    nate/v2-upgrade: Add all route tests

--- src/test/routes.test.js
+++ src/test/routes.test.js
@@ -27,34 +27,49 @@ describe('allStops', () => {
     const allStops = `${root}/allStops/`;
 
     test(allStops, () => request(server).get(allStops).expect(200));
+
+    // TODO
 });
 
 describe('alerts', () => {
     const alerts = `${root}/alerts/`;
 
     test(alerts, () => request(server).get(alerts).expect(200));
+
+    // TODO
 });
 
 describe('delay', () => {
     const delay = `${root}/delay/`;
 
     test(delay, () => request(server).get(delay).expect(200));
+
+    // TODO
 });
 
 describe('route', () => {
     const places = `${root}/route/`;
 
-    test(places, () => request(server).get(places).expect(200));
+    test(places, () => request(server).get(places).expect((res) => {
+        if (res.statusCode !== 200) throw new Error('Bad status code ', res.statusCode);
+        if (res.body.success === true) throw new Error('Empty request body returned successfully', res.statusCode);
+    }));
+
+    // TODO
 });
 
 describe('places', () => {
     const places = `${root}/places/`;
 
     test(places, () => request(server).get(places).expect(404));
+
+    // TODO
 });
 
 describe('tracking', () => {
     const places = `${root}/tracking/`;
 
     test(places, () => request(server).get(places).expect(404));
+
+    // TODO
 });
