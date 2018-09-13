f4de025d4fd86f0145231ffcbacbde5f837e34b1
JaredLim <jylim9823@gmail.com>
2018-09-12 20:51:01 -0400

    updated setup.sh: git clone only a single branch

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,28 +1,26 @@
 #!/usr/bin/env bash
 npm install
-git clone https://github.com/graphhopper/graphhopper.git
-git clone https://github.com/graphhopper/map-matching.git
-cd graphhopper
-git checkout 0.10
-cd ..
+git clone -b 0.10 https://github.com/graphhopper/graphhopper.git
+git clone -b 0.9 https://github.com/graphhopper/map-matching.git
+
 mkdir graphhopper-walking
 cd graphhopper-walking
-git clone https://github.com/graphhopper/graphhopper.git
-cd graphhopper
-git checkout 0.10
-cd ..
+git clone -b 0.10 https://github.com/graphhopper/graphhopper.git
 cd ..
 
 cd map-matching
-git checkout 0.9
 ./map-matching.sh action=import datasource=../osrm/map.osm vehicle=car,foot
 cd ..
+
 wget https://s3.amazonaws.com/tcat-gtfs/tcat-ny-us.zip
+
 mkdir tcat-ny-us
 tar xvf tcat-ny-us.zip -C tcat-ny-us
+
 cd graphhopper
 ./graphhopper.sh buildweb
 cd ..
+
 cd graphhopper-walking/graphhopper
 ./graphhopper.sh buildweb
 cd ../..
