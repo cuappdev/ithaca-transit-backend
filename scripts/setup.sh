1355543a260ee6d3547679c3830e2413daf269cd
Nate <njs59@cornell.edu>
2018-09-21 06:45:04 -0400

    Exit service after graphs built and wait until service open

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,6 +7,8 @@
 
 echo $PWD
 
+# Check for required programs
+
 if ! [ -x "$(command -v mvn)" ]; then
   echo 'Error: mvn (Maven) is not installed. Install then try again.' >&2
   exit 1
@@ -42,34 +44,25 @@ if ! [ -e ".env" ]; then
     cp env.template .env
 fi
 
+
+# Check for / install required npm modules
+
 echo "Installing npm modules..."
 npm install
 
-if ! ( [ -d "graphhopper" ] && ! [ -x "$(git -C graphhopper rev-parse)" ] ); then
-    echo "Cloning graphhopper repository..."
-    git clone -b 0.10 https://github.com/graphhopper/graphhopper.git
-fi
+# Import caches and other data
 
-if ! ( [ -d "map-matching" ] && ! [ -x "$(git -C map-matching rev-parse)" ] ); then
-    echo "Cloning map matching repository..."
-    git clone -b 0.9 https://github.com/graphhopper/map-matching.git
-fi
-
-if ! ( [ -d "graphhopper-walking" ] && ! [ -x "$(git -C graphhopper-walking rev-parse)" ] ); then
-    echo "Cloning graphhopper-walking repository..."
-    mkdir graphhopper-walking
-    cd graphhopper-walking
-    git clone -b 0.10 https://github.com/graphhopper/graphhopper.git
+# if no orsm directory or the osrm data repo is not present
+if ( ! [ -d "osrm" ] || [ -z "$(ls -p osrm | grep -v /)" ] ); then
+    echo "Downloading osrm data..."
+    mkdir osrm
+    cd osrm
+    # git clone --single-branch -b tcat-map https://github.com/cuappdev/ithaca-transit-backend.git
+    curl -L -O https://github.com/cuappdev/ithaca-transit-backend/raw/tcat-map/map.osm
     cd ..
 fi
 
-if ( ! [ -d "map-matching" ] || [ -z "$(ls -p map-matching | grep -v /)" ] ); then
-    echo "Setting up map-matching repository..."
-    cd map-matching
-    ./map-matching.sh action=import datasource=../osrm/map.osm vehicle=car,foot
-    cd ..
-fi
-
+# if no cache directory and the tcat data is not present
 if ! ( [ -d "cache" ] && [ -e "cache/tcat-ny-us.zip" ] ); then
     echo "Downloading TCAT data..."
     mkdir cache
@@ -78,12 +71,49 @@ if ! ( [ -d "cache" ] && [ -e "cache/tcat-ny-us.zip" ] ); then
     cd ..
 fi
 
+# if no cache directory or the tcat data is not present
 if ( ! [ -d "tcat-ny-us" ] || [ -z "$(ls -p tcat-ny-us | grep -v /)" ] ); then
     echo "Unzipping TCAT data..."
     mkdir tcat-ny-us
     unzip -o cache/tcat-ny-us.zip -d tcat-ny-us
 fi
 
+
+# Check for / clone required dependencies as git repos
+
+# if no graphhopper directory or the repo is not already present
+if ( ! [ -d "graphhopper" ] || [ -z "$(ls -p graphhopper | grep -v /)" ] ); then
+    echo "Cloning graphhopper repository..."
+    git clone --single-branch -b 0.10 https://github.com/graphhopper/graphhopper.git
+fi
+
+# if no map-matching directory or the repo is not present
+if ( ! [ -d "map-matching" ] || [ -z "$(ls -p map-matching | grep -v /)" ] ); then
+    echo "Cloning map matching repository..."
+    git clone --single-branch -b 0.9 https://github.com/graphhopper/map-matching.git
+fi
+
+# if no graphhopper-walking directory or the repo is not present
+if ( ! [ -d "graphhopper-walking" ] || [ -z "$(ls -p graphhopper-walking | grep -v /)" ] ); then
+    echo "Cloning graphhopper-walking repository..."
+    mkdir graphhopper-walking
+    cd graphhopper-walking
+    git clone -b 0.10 https://github.com/graphhopper/graphhopper.git
+    cd ..
+fi
+
+
+# Setup/build dependencies
+
+# if no map-matching or the directory is empty
+if ( ! [ -d "map-matching" ] || [ -z "$(ls -p map-matching | grep -v /)" ] ); then
+    echo "Setting up map-matching repository..."
+    cd map-matching
+    ./map-matching.sh action=import datasource=../osrm/map.osm vehicle=car,foot
+    cd ..
+fi
+
+# if no graphhopper build target directory or the directory is empty
 if ( ! [ -d "graphhopper/web/target" ]  || [ -z "$(ls -p graphhopper/web/target | grep -v /)" ] ); then
     echo "Building graphhopper service..."
     cd graphhopper
@@ -91,6 +121,7 @@ if ( ! [ -d "graphhopper/web/target" ]  || [ -z "$(ls -p graphhopper/web/target 
     cd ..
 fi
 
+# if no graphhopper-walking build target directory or the directory is empty
 if ( ! [ -d "graphhopper-walking/graphhopper/web/target" ] || [ -z "$(ls -p graphhopper-walking/graphhopper/web/target | grep -v /)" ] ); then
     echo "Building graphhopper-walking service..."
     cd graphhopper-walking/graphhopper
@@ -98,43 +129,57 @@ if ( ! [ -d "graphhopper-walking/graphhopper/web/target" ] || [ -z "$(ls -p grap
     cd ../..
 fi
 
+
+# Run graph creation/update for graphhopper services
+
+# if no graph-cache directory or the directory is empty
 if ( ! [ -d "graph-cache" ] || [ -z "$(ls -p graph-cache | grep -v /)" ] ); then
     echo "Building graph cache..."
     java -Xmx1g -Xms1g -jar graphhopper/web/target/graphhopper-web-*-with-dep.jar datareader.file=osrm/map.osm gtfs.file=tcat-ny-us.zip jetty.port=8988 jetty.resourcebase=./graphhopper/web/src/main/webapp graph.flag_encoders=pt prepare.ch.weightings=no graph.location=./graph-cache &
 
     # wait until open
-    while ! echo exit | nc localhost 8988; do
-        echo .
+     until nc -G 30 -z localhost 8988; do
+        echo "..."
         sleep 1
     done
 
+    npm run cleanup
 fi
 
+# if no walking graph-cache directory or the directory is empty
 if ( ! [ -d "graphhopper-walking/graph-cache" ] || [ -z "$(ls -p graphhopper-walking/graph-cache | grep -v /)" ] ); then
     echo "Building walking graph cache..."
     java -Xmx1g -Xms1g -jar graphhopper-walking/graphhopper/web/target/graphhopper-web-*-with-dep.jar datareader.file=osrm/map.osm jetty.port=8987 jetty.resourcebase=./graphhopper/web/src/main/webapp graph.flag_encoders=foot prepare.ch.weightings=no graph.location=graphhopper-walking/graph-cache &
 
     # wait until open
-    while ! echo exit | nc localhost 8987; do
-        echo .
+    until nc -G 30 -z localhost 8987; do
+        echo "..."
         sleep 1
     done
 
+    npm run cleanup
 fi
 
+
+# Start graphhopper service and finish
+
+# if no graphhopper/java processing running
 if ! [ $(ps aux | grep -E -c 'graphhopper|java') -gt 1 ]; then
 
     echo "Starting graphhopper..."
 
-    java -Xmx1g -Xms1g -jar graphhopper/web/target/graphhopper-web-*-with-dep.jar datareader.file=osrm/map.osm gtfs.file=tcat-ny-us.zip jetty.port=8988 jetty.resourcebase=./graphhopper/web/src/main/webapp graph.flag_encoders=pt prepare.ch.weightings=no graph.location=./graph-cache &>/dev/null &
-    java -Xmx1g -Xms1g -jar graphhopper-walking/graphhopper/web/target/graphhopper-web-*-with-dep.jar datareader.file=osrm/map.osm jetty.port=8987 jetty.resourcebase=./graphhopper/web/src/main/webapp graph.flag_encoders=foot prepare.ch.weightings=no graph.location=graphhopper-walking/graph-cache &>/dev/null &
+    java -Xmx1g -Xms1g -jar graphhopper/web/target/graphhopper-web-*-with-dep.jar datareader.file=osrm/map.osm gtfs.file=tcat-ny-us.zip jetty.port=8988 jetty.resourcebase=./graphhopper/web/src/main/webapp graph.flag_encoders=pt prepare.ch.weightings=no graph.location=./graph-cache & #&>/dev/null
+    java -Xmx1g -Xms1g -jar graphhopper-walking/graphhopper/web/target/graphhopper-web-*-with-dep.jar datareader.file=osrm/map.osm jetty.port=8987 jetty.resourcebase=./graphhopper/web/src/main/webapp graph.flag_encoders=foot prepare.ch.weightings=no graph.location=graphhopper-walking/graph-cache & #&>/dev/null
 
     cd map-matching
     ./map-matching.sh action=start-server &>/dev/null &
     cd ..
 
     # wait until open
-    sleep 3
+    until nc -G 30 -z localhost 8987 & nc -G 30 -z localhost 8988; do
+        echo "..."
+        sleep 1
+    done
 
 else
 
