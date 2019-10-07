b51c4051dd8a0146819bde777403c31a04a861f3
alanna-zhou <alanna.zhou@gmail.com>
2019-10-07 09:44:04 -0400

    Set up delays endpoint (#268)

--- src/API.js
+++ src/API.js
@@ -20,6 +20,7 @@ class API extends ApplicationAPI {
       Routers.ApplePlacesRouter,
       Routers.AppleSearchRouter,
       Routers.DelayRouter,
+      Routers.DelaysRouter,
       Routers.HelloWorldRouter,
       Routers.MultiRouteRouter,
       Routers.PlacesAutocompleteRouter,
