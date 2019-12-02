d03f70c2f58c448e7d20beb1dd6873ef4198e7fa
Omar Rasheed <oyr2@cornell.edu>
2019-12-02 14:48:35 -0500

    Add documentation too

--- src/utils/NotificationUtils.js
+++ src/utils/NotificationUtils.js
@@ -1,12 +1,10 @@
 // @flow
 function notifyForDelays(deviceToken: string, stopID: String, tripID: String, uid: string) {
   // TODO: Implement
-  // return true;
 }
 
 function notifyForDeparture(deviceToken: string, minutesBefore: number, stopID: String, tripID: String, uid: string) {
   // TODO: Implement
-  // return true;
 }
 
 export default {
